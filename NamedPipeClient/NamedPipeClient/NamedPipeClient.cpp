cf0428c3248bef6e2b2b28fc052acccfc2ad6adb
Suraj Pandey <me.suraj.pandey@gmail.com>
2018-10-07 06:54:25 -1000

    Created a new Cmake Project

---
+++ NamedPipeClient/NamedPipeClient/NamedPipeClient.cpp
@@ -0,0 +1,12 @@
+﻿// NamedPipeClient.cpp : Defines the entry point for the application.
+//
+
+#include "NamedPipeClient.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
